35182390a39071c632bf8dae56d16b380d247adf
Ashley Smith <smith_ashley12@students.columbusstate.edu>
2025-01-19 22:30:11 -0500

    First commit, assignment 1

---
+++ First/src/main/java/com/examples/first/First.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.examples.first;
+
+/**
+ *
+ * @author AM
+ */
+public class First {
+
+    public static void main(String[] args) {
+        
+        String jobTitle = "Information Technology Anaylsts";
+        String reasons = "To resolve technical problems";
+        String median_income = "$120,360";
+        System.out.println("Hola mundo!");
+        System.out.println("Job Title: " + jobTitle);
+        System.out.println("Why: " + reasons);
+        System.out.print("Median Salary: " + median_income);
+        
+    }
+}
