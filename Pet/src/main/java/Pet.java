2a356064509c51f1a29925a7dec0804f58ba5e88
sashley86 <smith_ashley12@students.columbusstate.edu>
2025-02-07 16:40:19 -0500

    second commit

--- Pet/src/main/java/Pet.java
+++ Pet/src/main/java/Pet.java
@@ -1,37 +1,30 @@
-/**
- *
- * @author AM
- */
 public class Pet {
     
     //private data field
     private String name;
     
-    //public Constructors
-    public Pet (String name) {
+    //default constructor
+    public Pet(String name) {
         this.name = name;
-        System.out.println("Pet Information: ");
     }
-    //public get or accessor method for every private data field(except if it is a secret)
+    //public get(accessor) or set(mutator) method for every private data field(except if it is a secret or read-only)
     public String getName() {
         return name;
     }
-    //public set or mutator method for every private data field(except when the data field is read-only)
     public void setName(String newName) {
         this.name = newName;
-    }    
+    }
     //public toString method that prints out all of the object state(meaning the data in the object instance that you will later instantiate in the main[] method)
     public String toString() {
-        String output = "Name: ";
-        output += this.getName();
-        return output;
+        return "Pet Information:" + "\n" + "Name: " + name + "\n";
     }
-    // main
+    // main method
     public static void main (String[] args) {
         //instantiate Pet
-        Pet myPet = new Pet("Pet Name");
-        System.out.println(myPet);
-        Pet myPet2 = new Pet ("Buster");
+        Pet myPet1 = new Pet("Pet Name");
+        Pet myPet2 = new Pet(" ");
+        myPet2.setName("Hyleigh");
+        System.out.println(myPet1);
         System.out.println(myPet2);
     }
 }
